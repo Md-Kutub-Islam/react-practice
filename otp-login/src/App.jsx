5d298f80d52d68b7bc8638f335b276fbe2b061d5
Md-Kutub-Islam <mdkutubislam09@gmail.com>
2024-08-04 16:57:01 +0530

    otp-login

---
+++ otp-login/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import LoginForm from "./components/LoginForm";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Login with phone</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
