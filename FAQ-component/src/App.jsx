d77a660f1716821151ed26991ecf4e364422d6ec
Md-Kutub-Islam <mdkutubislam09@gamil.com>
2024-08-07 16:40:40 +0530

    FAQ-component

---
+++ FAQ-component/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FaqCpmonent from "./component/FaqCpmonent";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Frequently ask question</h1>
+      <FaqCpmonent />
+    </div>
+  );
+}
+
+export default App;
